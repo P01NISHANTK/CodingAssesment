001f40973ed2d0894bf6c3beb112c56abf701736
Nishant Kumar <n.kumar3@zensar.com>
2022-04-30 21:02:46 +0530

    Initial commit

---
+++ app/src/main/java/com/discoverry/codingassesment/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.discoverry.codingassesment.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.discoverry.codingassesment.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
